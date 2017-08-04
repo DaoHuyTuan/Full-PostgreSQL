684089d29b3a8baed7076724df21f2291bacae33
dao huy tuan <tuandh95@gmail.com>
2017-08-04 18:03:27 +0700

    done insert

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const parser = require('body-parser').urlencoded({ extended: false });
+
+
+const queryDB = require('./db');
 app.set("view engine" , "ejs");
 app.set("views" , "./views");
 app.use(express.static('public'));
 
 
-app.get("/", (req,res) => {
-    res.render("home");
-})
+
+app.get("/",(req,res) => {
+    queryDB('SELECT * FROM "Product"', (err, data) => {
+        if(err) return console.log(err.toString());
+        res.render("home",{mang: data.rows});
+        });
+    });
+    
+
+
 app.listen(3000);
